c1a039b7051cd73d67d6a54726f6fef783c6ee04
Thomas Hartmann <thomas@webtrade.dk>
2023-01-17 16:08:48 +0100

    now with typescript

--- README.md
+++ README.md
@@ -9,7 +9,10 @@
 3. Start simple
   - change file extension to `.ts` and `.tsx`
   - add type annotations
-  
+    - create a type: `type TemperatureUpdaterProps = { temperature?: number, setTemperature?: React.ChangeEventHandler<HTMLInputElement> //React.Dispatch<React.SetStateAction<number>> }`
+    - use this type in both Fahrenheit and Celsius components.
+    - the update method takes an event argument: `event: React.ChangeEvent<HTMLInputElement> `
+    - evt.target.value is a string, so we need to convert it to a number: `const temperature = Number(evt.target.value)`.
 4. Convert all files
 5. Increase strictness
 6. Clean it up
--- src/components/converter/converter.tsx
+++ src/components/converter/converter.tsx
@@ -1,29 +1,38 @@
 import React, {useState, useEffect} from "react"
 import './converter.css'
 
+type TemperatureUpdaterProps = {
+    temperature?: number,
+    setTemperature?: React.ChangeEventHandler<HTMLInputElement> //React.Dispatch<React.SetStateAction<number>>
+}
+
+
 export default ()=>{
     const [temperature, setTemperature] = useState(0);
-    const update =(event) => {
-        const fahrenheit = event.target.value;
+
+    const update =(event: React.ChangeEvent<HTMLInputElement> ):void => {
+        const fahrenheit:number = Number.parseFloat(event.target.value);
         const celcius = (fahrenheit - 32) * 5/9;
         setTemperature(celcius);
     }
     
-    return(<div className="greenborder">
-        <FahrenheitComponent update={update}></FahrenheitComponent>
+    return(
+    <div className="greenborder">
+        <FahrenheitComponent setTemperature={update}></FahrenheitComponent>
         <CelciusComponent temperature={temperature}></CelciusComponent>
     </div>);
     
 }
 
-const FahrenheitComponent = (props)=>{
-    return (<div className="redborder">
-        <input type="text" onChange={props.update} placeholder="Enter temperature in fahrenheit"/>
+const FahrenheitComponent = ({setTemperature}:TemperatureUpdaterProps)=>{
+    return (
+    <div className="redborder">
+        <input type="text" onChange={setTemperature} placeholder="Enter temperature in fahrenheit"/>
     </div>);
 }
 
-const CelciusComponent= (props) => {
-    return <div className="blueborder">Temperature in Celcius:{" "}
-        {props.temperature}
+const CelciusComponent = ({temperature}:TemperatureUpdaterProps) => {
+    return <div className="blueborder">
+        Temperature in Celcius:{" "} {temperature}
     </div>
 }
